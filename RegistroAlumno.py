d045c27aa67677e394e964cc657f29f4f4f88958
Alberto Rojas <albertorojasm95@gmail.com>
2015-02-16 15:40:08 -0600

    Programa capaz de cumplir su objetivo de buscar en un registro, y agregado una corrida de escritorio.

--- RegistroAlumno.py
+++ RegistroAlumno.py
@@ -4,9 +4,10 @@ import os
 
 materias=['Administracion','Diseño','Dinamica','Estructura',\
           'Ingles','Metodos','Practicas','Redes']
-
 grupo=[0 for i in range(40)]
 
+
+#REGISTROS/CLASES
 class nacimiento(record):
     dia=1
     mes=1
@@ -19,7 +20,8 @@ class alumno(record):
     a_am=''
     fecha=None
     calificaciones=[[0 for i in range(5)] for i in range(8)]
-
+    
+#IMPRESION
 def impresion(alumno):
     print 'ID: '+ str(alumno.a_id)
     print 'Nombre: '+alumno.a_nom
@@ -27,33 +29,33 @@ def impresion(alumno):
     print 'Apellido Materno: '+alumno.a_am
     print 'Fecha de Nacimiento: '+alumno.fecha.dia+'/'\
           +alumno.fecha.mes+'/'+alumno.fecha.anio
-    #for i in range(8):
-    #    acumulador=0
-    #    promedio=0
-    #    for j in range(4):
-    #        acumulador=alumno.calificaciones[i][j]+acumulador
-    #        promedio=acumulador/4
-    #        alumno.calificaciones[i][4]=promedio
-    #for i in range(8):
-    #    print 'Promedio de '+materias[i]+': '+str(alumno.calificaciones[i][4])
+    for i in range(8):
+        acumulador=0
+        promedio=0
+        for j in range(4):
+            acumulador=alumno.calificaciones[i][j]+acumulador
+            promedio=acumulador/4
+            alumno.calificaciones[i][4]=promedio
+    for i in range(8):
+        print 'Promedio de '+materias[i]+': '+str(alumno.calificaciones[i][4])
     return
 
-def busquedaid(id0, n, *grupo):
+#FUNCIONES
+def FINDid(ID, k, *grupo):
     encontrados=0
-    for i in range(n):
-        if id0==grupo[i].a_id:
+    for i in range(k):
+        if ID==grupo[i].a_id:
             ubicacion=i
             encontrados=encontrados+1
-    if encontrados <= 0:
+            print 'Encontrado!'
+            impresion(grupo[ubicacion])
+    if encontrados<=0:
         print 'No existen coincidencias'
-    else:
-        print 'Encontrado!'
-        impresion(grupo[ubicacion])
     return
 
-def busquedanombre(nombre, n, *grupo):
+def FINDname(nombre, k, *grupo):
     encontrados=0
-    for i in range(n):        
+    for i in range(k):        
         if nombre==grupo[i].a_nom:
             ubicacion=i
             print 'Encontrado!'
@@ -63,88 +65,86 @@ def busquedanombre(nombre, n, *grupo):
         print 'No existen coincidencias'
     return
 
-def busquedaedad(age, n, *grupo):
-    for i in range(n):
-        encontrados=0
+def FINDage(age, k, *grupo):
+    encontrados=0
+    for i in range(k):        
         edad=grupo[i].fecha.dia+'/'+grupo[i].fecha.mes+'/'+grupo[i].fecha.anio
         if age == edad:
             ubicacion=i
             encontrados=encontrados+1
-        if encontrados <=0:
-            print 'No existen coincidencias'
-        if encontrados > 0:
             print 'Encontrado!'
             impresion(grupo[ubicacion])
+    if encontrados <=0:
+        print 'No existen coincidencias'           
     return
        
-n=int(raw_input("Ingrese el tamaño del grupo: "))
+k=int(raw_input("Ingrese el tamaño del grupo: "))
 
-for h in range(n):
-    grupo[h]=alumno(a_id=int(raw_input('Ingrese id: ')),\
+#LLENADO
+for l in range(k):
+    print '\n'
+    grupo[l]=alumno(a_id=int(raw_input('Ingrese id: ')),\
                     a_nom=raw_input('Ingrese nombre: '),\
                     a_ap=raw_input('Ingrese apeliido paterno: '),\
                     a_am=raw_input('Ingrese apeliido materno: '),\
                     fecha=nacimiento(dia=raw_input('Ingrese dia de nacimiento: '),\
                                      mes=raw_input('Ingrese mes de nacimiento: '),\
-                                     anio=raw_input('Ingrese año de nacimiento 4 digitos: '))
-                    )
+                                     anio=raw_input('Ingrese año de nacimiento 4 digitos: ')))
     
-    grupo[h].a_nom=grupo[h].a_nom.capitalize()
-    grupo[h].a_ap=grupo[h].a_ap.capitalize()
-    grupo[h].a_am=grupo[h].a_am.capitalize()
+    grupo[l].a_nom=grupo[l].a_nom.capitalize()
+    grupo[l].a_ap=grupo[l].a_ap.capitalize()
+    grupo[l].a_am=grupo[l].a_am.capitalize()
     
-    dialen=len(grupo[h].fecha.dia)
-    if dialen<2:
-        grupo[h].fecha.dia='0'+grupo[h].fecha.dia
+    LEN=len(grupo[l].fecha.dia)
+    if LEN<2:
+        grupo[l].fecha.dia='0'+grupo[l].fecha.dia
 
-    meslen=len(grupo[h].fecha.mes)
-    if meslen<2:
-        grupo[h].fecha.mes='0'+grupo[h].fecha.mes
+    LEN=len(grupo[l].fecha.mes)
+    if LEN<2:
+        grupo[l].fecha.mes='0'+grupo[l].fecha.mes
     
-    '''for i in range(8):
+    for i in range(8):
         for j in range(4):
-            grupo[h].calificaciones[i][j]=int(raw_input('Ingrese calificacion '+str(j+1)+\
-                                                        ' de la materia '+materias[i]+': '))'''
-    impresion(grupo[h])
-    print '\n' 
-
-   
-
+            grupo[l].calificaciones[i][j]=int(raw_input('Ingrese calificacion '+str(j+1)+\
+                                                        ' de la materia '+materias[i]+': '))
+    print '\n'
+    impresion(grupo[l])
+     
+#BUSQUEDA
 programa='s'
 while(programa=='s'):
+    print '\n'
     print 'BUSQUEDA\n1.- ID\n2.- Nombre\n3.- Fecha\n'
     valor=int(raw_input('Seleccione su metodo de busqueda: '))
     if valor==1:
-        print 'BUSQUEDA POR ID'
-        id0=int(raw_input('Ingrese ID: '))
-        busquedaid(id0, n, *grupo)
+        print '\nBUSQUEDA POR ID'
+        ID=int(raw_input('Ingrese ID: '))
         print '\n'
+        FINDid(ID, k, *grupo)
         
     elif valor==2:
-        print 'BUSQUEDA POR NOMBRE'
+        print '\nBUSQUEDA POR NOMBRE'
         nombre=raw_input('Ingrese Nombre: ')
         nombre=nombre.capitalize()
-        busquedanombre(nombre, n, *grupo)
         print '\n'
+        FINDname(nombre, k, *grupo)
         
     elif valor==3:
-        print 'BUSQUEDA POR NACIMIENTO'
+        print '\nBUSQUEDA POR NACIMIENTO'
         day=raw_input('Ingrese Dia: ')
         month=raw_input('Ingrese Mes: ')
         year=raw_input('Ingrese Año 4 digitos: ')
-        daylen=len(day)
-        if daylen<2:
+        LEN=len(day)
+        if LEN<2:
             day='0'+day
 
-        monthlen=len(month)
-        if monthlen<2:
+        LEN=len(month)
+        if LEN<2:
             month='0'+month
 
         age=day+'/'+month+'/'+year
-        busquedaedad(age, n, *grupo)
         print '\n'
-
+        FINDage(age, k, *grupo)
         
-
+    print '\n'
     programa=raw_input('¿Desea seguir buscando? (s/n)')
-
